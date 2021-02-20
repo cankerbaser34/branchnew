f5191d852217e6b02406dfc989faee583f4b478f
cankerbaser34 <cihanbaser@yahoo.com>
2021-02-20 22:46:33 +0100

    first commit

---
+++ src/main/java/mavenpackage/FirstSeleniim.java
@@ -0,0 +1,31 @@
+package mavenpackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FirstSeleniim {
+
+  public static void main(String[] args) throws InterruptedException {
+
+    WebDriverManager.chromedriver().setup();
+
+    WebDriver driver = new ChromeDriver();
+
+    driver.manage().window().maximize();
+    driver.get("https://login.salesforce.com");
+
+    driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("name");
+
+    driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("location");
+
+    driver.findElement(By.name("Login")).click();
+    Thread.sleep(3000);
+
+    // driver.quit();
+  }
+
+}
